0511ad42eeb68469894104c86eabc9e4dc0b1d8e
Douglas dos Santos Brito <dbrito@uolinc.com>
2024-12-06 14:55:08 -0300

    Novo Logo + mock do chat

--- website/static/scripts.js
+++ website/static/scripts.js
@@ -5,7 +5,7 @@ document.getElementById('sendButton').addEventListener('click', function() {
     if (message !== "") {
         const messageDiv = document.createElement('div');
         messageDiv.classList.add('message');
-        messageDiv.innerHTML = `<span>Usuário:</span> ${message}`;
+        messageDiv.innerHTML = `<span class="you">Usuário:</span> ${message}`;
         document.getElementById('chatBox').appendChild(messageDiv);
         messageInput.value = '';  // Limpa o campo de texto
         document.getElementById('chatBox').scrollTop = document.getElementById('chatBox').scrollHeight; // Desce automaticamente
